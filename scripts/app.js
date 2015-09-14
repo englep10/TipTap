d08fc199bb6f93058b2329a37b36db82d4d60b18
Philip Engles <philipengles@Philips-MacBook-Pro-2.local>
2015-09-13 21:08:17 -0400

    initial commit

---
+++ scripts/app.js
@@ -0,0 +1,30 @@
+'use strict';
+
+var app = angular
+  .module('TaskNinjaApp', [
+    'ngAnimate',
+    'ngResource',
+    'ngRoute',
+    'firebase'
+  ])
+  .constant('FURL', 'https://tiptap.firebaseio.com/')
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html'
+      })
+      .when('/post', {
+        templateUrl: 'views/post.html',
+        controller: "TaskController"
+      })
+      .when('/edit', {
+        templateUrl: 'views/edit.html'
+      })
+      .when('/browse', {
+        templateUrl: 'views/browse.html',
+        controller: 'TaskController'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
