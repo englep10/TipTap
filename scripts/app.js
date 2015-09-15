79c477c6e0c50ad1832e918567c88cc584010df7
Philip Engles <philipengles@Philips-MacBook-Pro-2.local>
2015-09-15 00:55:53 -0400

    created authentication for users as well as login capabilities

--- scripts/app.js
+++ scripts/app.js
@@ -13,12 +13,21 @@ var app = angular
       .when('/', {
         templateUrl: 'views/main.html'
       })
+      .when('/login', {
+        templateUrl: 'views/login.html',
+        controller: "AuthController"
+      })
+      .when('/register', {
+        templateUrl: 'views/register.html',
+        controller: "AuthController"
+      })
       .when('/post', {
         templateUrl: 'views/post.html',
         controller: "TaskController"
       })
-      .when('/edit', {
-        templateUrl: 'views/edit.html'
+      .when('/edit/:taskId', {
+        templateUrl: 'views/edit.html',
+        controller: "TaskController"
       })
       .when('/browse', {
         templateUrl: 'views/browse.html',
